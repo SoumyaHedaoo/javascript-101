a39614419b2e225f28b17afbbcc5af1e641f8070
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2025-07-03 15:49:39 +0530

    coversions

---
+++ 02-conversions.js
@@ -0,0 +1,42 @@
+let num = "33abc";
+
+
+let numInInt = Number(num);
+
+
+console.log(typeof  numInInt);
+
+
+/*
+functions :
+
+Number , String , Boolean .....
+
+
+Numbers : 
+
+    "33" ->33
+    "33abs" ->NaN
+    null ->0
+    .
+    .
+    .
+    .
+
+
+
+    check for these rules evertime you use it
+
+Boolean :
+    1 -> true
+    0->false
+
+    ""->false
+    "aefe"->true
+    ..
+    .
+    .
+    .
+    .
+
+*/
